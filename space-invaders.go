e192e97124908cbc39be315825b494ce2c91ec16
MiltonEBR <A01634505@itesm.mx>
2021-11-21 19:32:15 -0600

    Simple alien generation implemented

--- space-invaders.go
+++ space-invaders.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-//Defining the structs
+// Defining the structs
 type Cell struct {
 	x int
 	y int
@@ -23,7 +23,7 @@ type Alien struct {
 
 type Game struct{}
 
-//Global variables
+// Global variables
 const (
   WindowH = 640
   WindowW = 420
@@ -31,8 +31,8 @@ const (
 )
 
 var (
-  grid [][]int //Size gets initialized on main
-  alienTest Alien//This will become an array of aliens
+  grid [][]int
+  aliens []Alien
   alienSprite *ebiten.Image
 )
 
@@ -40,7 +40,7 @@ func initGrid(){
   rows := int(math.Floor(WindowH/CellSize))
   cols := int(math.Floor(WindowW/CellSize))
   
-  fmt.Printf("%dx%d grid created\n",rows,cols)
+  fmt.Printf("%dx%d grid created\n",cols,rows)
   
   grid = make([][]int, rows)
   for i := range grid {
@@ -48,15 +48,43 @@ func initGrid(){
   }
 }
 
-func initAliens(){
+func initAliens(amount int){
+  // Alien maximum value is 50% of the grid, this logic may be moved later
+  cappedAmount := int(math.Min(float64(amount),math.Floor(float64(len(grid)*len(grid[0]))/2)))
+
+  // Sprite
   alienSprite = ebiten.NewImage(CellSize,CellSize)
   debugColor:= color.RGBA{uint8(255), 0, 0, uint8(255)}
   alienSprite.Fill(debugColor)
-  
-  alienTest = Alien{
-    id: 0,
-    pos: Cell{x:10,y:20},
-    sprite: alienSprite,
+
+  aliens = make([]Alien, cappedAmount)
+
+  curRow := 0
+  curCol := 0
+
+  for i:= range aliens{
+    aliens[i] = Alien{
+      id:i,
+      pos: Cell{x:curCol,y:curRow},
+      sprite: alienSprite,
+    }
+    grid[curRow][curCol] = i
+    if curCol>0 && curCol%(len(grid[0])-1) == 0 {
+      curRow++
+      curCol=0
+    }else{
+      curCol++
+    }
+  }
+
+  fmt.Printf("Generated %d aliens\n",len(aliens))
+}
+
+func drawAliens(screen *ebiten.Image){
+  for i:= range aliens{
+    options := new(ebiten.DrawImageOptions)
+    options.GeoM.Translate(float64(aliens[i].pos.x*CellSize),float64(aliens[i].pos.y*CellSize))
+    screen.DrawImage(aliens[i].sprite,options)
   }
 }
 
@@ -66,14 +94,11 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-  options := new(ebiten.DrawImageOptions)
-  options.GeoM.Translate(float64(alienTest.pos.x),float64(alienTest.pos.y))
-  //ebitenutil.DrawRect(screen,float64(alienTest.pos.x),float64(alienTest.pos.y),CellSize,CellSize,color.White)
-  screen.DrawImage(alienTest.sprite,options)
+  drawAliens(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-  return 320, 240
+  return WindowW, WindowH
 }
 
 func main() {
@@ -83,8 +108,9 @@ func main() {
   
   initGrid()
   
-  initAliens()
-  
+  initAliens(50)
+
+
   if err := ebiten.RunGame(game); err != nil {
     log.Fatal(err)
   }
